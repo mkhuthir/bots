a78705281cb91d07545ea0093991230588f68c64
mkhuthir <mkhuthir@gmail.com>
2022-04-10 10:00:51 +0400

    u

---
+++ scripts/urdf_to_sdf.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Muthanna Alwahash 2022
+
+echo 'This script will convert URDF to SDF.'
+
+echo 'golem'
+gz sdf -p ../urdf/golem.urdf            > ../models/golem/model.sdf
+echo 'pxm3'
+gz sdf -p ../urdf/pxm3.urdf             > ../models/pxm3/model.sdf
+echo 'razbot'
+gz sdf -p ../urdf/razbot.urdf           > ../models/razbot/model.sdf
+echo 'tb3_burger'
+gz sdf -p ../urdf/tb3_burger.urdf       > ../models/tb3_burger/model.sdf
+echo 'tb3_waffle'
+gz sdf -p ../urdf/tb3_waffle.urdf       > ../models/tb3_waffle/model.sdf
+echo 'tn3_waffle_pi'
+gz sdf -p ../urdf/tb3_waffle_pi.urdf    > ../models/tb3_waffle_pi/model.sdf
+echo 'twr'
+gz sdf -p ../urdf/twr.urdf              > ../models/twr/model.sdf
+echo 'Done!'
---
+++ scripts/xacro_to_urdf.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Muthanna Alwahash 2022
+
+echo 'This script will convert from XACRO to URDF.'
+
+echo 'golem'
+xacro ../xacro/golem.xacro          > ../urdf/golem.urdf
+echo 'pxm3'
+xacro ../xacro/pxm3.xacro           > ../urdf/pxm3.urdf
+echo 'razbot'
+xacro ../xacro/razbot.xacro         > ../urdf/razbot.urdf
+echo 'tb3_burger'
+xacro ../xacro/tb3_burger.xacro     > ../urdf/tb3_burger.urdf
+echo 'tb3_waffle'
+xacro ../xacro/tb3_waffle.xacro     > ../urdf/tb3_waffle.urdf
+echo 'tn3_waffle_pi'
+xacro ../xacro/tb3_waffle_pi.xacro  > ../urdf/tb3_waffle_pi.urdf
+echo 'twr'
+xacro ../xacro/twr.xacro            > ../urdf/twr.urdf
+echo 'Done!'
