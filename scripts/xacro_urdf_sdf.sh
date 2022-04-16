f3d64370e5d866fb590b058339bd50e5eb4fee16
mkhuthir <mkhuthir@gmail.com>
2022-04-16 22:08:34 +0400

    u

--- scripts/xacro_urdf_sdf.sh
+++ scripts/xacro_urdf_sdf.sh
@@ -1,51 +1,42 @@
-#!/bin/sh
-
+#! /bin/bash
 # Muthanna Alwahash 2022
+# This script will convert XACRO files to URDF then URDF files to SDF
 
-echo 'This script will convert from XACRO to URDF.'
+# locations of files
+xacro="src/robot_gazebo/xacro/"
+urdf="src/robot_gazebo/urdf/"
+sdf="src/robot_gazebo/models/"
 
-echo 'golem'
-xacro src/robot_gazebo/xacro/golem.xacro          > src/robot_gazebo/urdf/golem.urdf
-echo 'kr3r540'
-xacro src/robot_gazebo/xacro/kr3r540.xacro          > src/robot_gazebo/urdf/kr3r540.urdf
-echo 'kr5_arc'
-xacro src/robot_gazebo/xacro/kr5_arc.xacro          > src/robot_gazebo/urdf/kr5_arc.urdf
-echo 'kr16_2'
-xacro src/robot_gazebo/xacro/kr16_2.xacro          > src/robot_gazebo/urdf/kr16_2.urdf
-echo 'pxm3'
-xacro src/robot_gazebo/xacro/pxm3.xacro           > src/robot_gazebo/urdf/pxm3.urdf
-echo 'razbot'
-xacro src/robot_gazebo/xacro/razbot.xacro         > src/robot_gazebo/urdf/razbot.urdf
-echo 'tb3_burger'
-xacro src/robot_gazebo/xacro/tb3_burger.xacro     > src/robot_gazebo/urdf/tb3_burger.urdf
-echo 'tb3_waffle'
-xacro src/robot_gazebo/xacro/tb3_waffle.xacro     > src/robot_gazebo/urdf/tb3_waffle.urdf
-echo 'tn3_waffle_pi'
-xacro src/robot_gazebo/xacro/tb3_waffle_pi.xacro  > src/robot_gazebo/urdf/tb3_waffle_pi.urdf
-echo 'twr'
-xacro src/robot_gazebo/xacro/twr.xacro            > src/robot_gazebo/urdf/twr.urdf
+# list of robots
+robots=(
+    golem
+    kr3r540
+    kr5_arc
+    kr16_2
+    kr120r2500pro
+    lbr_iiwa_14_r820
+    pxm3 razbot
+    tb3_burger
+    tb3_waffle
+    tb3_waffle_pi
+    twr
+)
+
+# Phase one: Xacro > URDF
+echo 'Starting to convert from XACRO to URDF......'
+for i in {0..11}
+do
+    echo "$((i+1))- ${robots[$i]}"
+    xacro "$xacro${robots[$i]}.xacro" > "$urdf${robots[$i]}.urdf"
+done
 echo 'Done!'
 
-echo 'This script will convert URDF to SDF.'
-
-echo 'golem'
-gz sdf -p src/robot_gazebo/urdf/golem.urdf            > src/robot_gazebo/models/golem/model.sdf
-echo 'kr3r540'
-gz sdf -p src/robot_gazebo/urdf/kr3r540.urdf           > src/robot_gazebo/models/kr3r540/model.sdf
-echo 'kr5_arc'
-gz sdf -p src/robot_gazebo/urdf/kr5_arc.urdf           > src/robot_gazebo/models/kr5_arc/model.sdf
-echo 'kr16_2'
-gz sdf -p src/robot_gazebo/urdf/kr16_2.urdf           > src/robot_gazebo/models/kr16_2/model.sdf
-echo 'pxm3'
-gz sdf -p src/robot_gazebo/urdf/pxm3.urdf             > src/robot_gazebo/models/pxm3/model.sdf
-echo 'razbot'
-gz sdf -p src/robot_gazebo/urdf/razbot.urdf           > src/robot_gazebo/models/razbot/model.sdf
-echo 'tb3_burger'
-gz sdf -p src/robot_gazebo/urdf/tb3_burger.urdf       > src/robot_gazebo/models/tb3_burger/model.sdf
-echo 'tb3_waffle'
-gz sdf -p src/robot_gazebo/urdf/tb3_waffle.urdf       > src/robot_gazebo/models/tb3_waffle/model.sdf
-echo 'tn3_waffle_pi'
-gz sdf -p src/robot_gazebo/urdf/tb3_waffle_pi.urdf    > src/robot_gazebo/models/tb3_waffle_pi/model.sdf
-echo 'twr'
-gz sdf -p src/robot_gazebo/urdf/twr.urdf              > src/robot_gazebo/models/twr/model.sdf
+# Phase two: URDF > SDF
+echo 'Starting to convert URDF to SDF.......'
+for i in {0..11}
+do
+    echo "$((i+1))- ${robots[$i]}"
+    gz sdf -p "$urdf${robots[$i]}.urdf" > "$sdf${robots[$i]}/model.sdf"
+done
 echo 'Done!'
+
