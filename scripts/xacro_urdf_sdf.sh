c0020cb84d373da4fb9a54ed367ac8ab83405d82
mkhuthir <mkhuthir@gmail.com>
2022-04-24 21:24:21 +0400

    u

--- scripts/xacro_urdf_sdf.sh
+++ scripts/xacro_urdf_sdf.sh
@@ -9,6 +9,7 @@ sdf="src/robot_gazebo/models/"
 
 # list of robots
 robots=(
+    diffbot
     golem
     kr3r540
     kr5_arc
@@ -26,7 +27,7 @@ robots=(
 
 # Phase one: Xacro > URDF
 echo 'Starting to convert from XACRO to URDF......'
-for i in {0..12} 
+for i in {0..13} 
 do
     echo "$((i+1))- ${robots[$i]}"
     xacro "$xacro${robots[$i]}.xacro" > "$urdf${robots[$i]}.urdf"
@@ -35,7 +36,7 @@ echo 'Done!'
 
 # Phase two: URDF > SDF
 echo 'Starting to convert URDF to SDF.......'
-for i in {0..12} 
+for i in {0..13} 
 do
     echo "$((i+1))- ${robots[$i]}"
     gz sdf -p "$urdf${robots[$i]}.urdf" > "$sdf${robots[$i]}/model.sdf"
