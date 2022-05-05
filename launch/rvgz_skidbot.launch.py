f7346e7640f5fa7e30347d223f8ae6e7287d3081
mkhuthir <mkhuthir@gmail.com>
2022-05-05 20:51:13 +0400

    u

--- launch/rvgz_skidbot.launch.py
+++ launch/rvgz_skidbot.launch.py
@@ -70,17 +70,17 @@ def generate_launch_description():
       parameters=[{ 'robot_description': Command(['xacro ', urdf_model])}]),
   
     # Publish the joint states of the robot
-    Node(
-      package=    'joint_state_publisher',
-      executable= 'joint_state_publisher',
-      name=       'joint_state_publisher',
-      condition=UnlessCondition(gui)),
+  #  Node(
+  #    package=    'joint_state_publisher',
+  #    executable= 'joint_state_publisher',
+  #    name=       'joint_state_publisher',
+  #    condition=UnlessCondition(gui)),
     
-    Node(
-      package=    'joint_state_publisher_gui',
-      executable= 'joint_state_publisher_gui',
-      name=       'joint_state_publisher_gui',
-      condition=IfCondition(gui)),
+  #  Node(
+  #    package=    'joint_state_publisher_gui',
+  #    executable= 'joint_state_publisher_gui',
+  #    name=       'joint_state_publisher_gui',
+  #    condition=IfCondition(gui)),
   
     # Launch RViz
     Node(
